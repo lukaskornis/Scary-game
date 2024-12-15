8cdb5033079d45bda7fa4837011916a212e73c49
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-12-15 14:46:08 +0200

    Enemy pathfinding

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -1,13 +1,33 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
+	public float viewDistance = 10f;
 	public float scareDistance = 3f;
 	public Transform target;
+	NavMeshAgent agent;
+
+	void Start()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+
+	void OnDrawGizmos()
+	{
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, viewDistance);
+	}
+
 
 	void Update()
 	{
 		float distance = Vector3.Distance(transform.position, target.position);
+
+		if (distance < viewDistance)
+		{
+			agent.SetDestination(target.position);
+		}
 
 		if (distance < scareDistance)
 		{
