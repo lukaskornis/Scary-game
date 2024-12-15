7d97ffa32c4f45a705c175e4fb2083690e069182
Lukas <39262485+lukaskornis@users.noreply.github.com>
2024-12-15 11:49:31 +0200

    Enemy AI

--- Assets/Monster.cs
+++ Assets/Monster.cs
@@ -1,12 +1,37 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Monster : MonoBehaviour
 {
+    public float viewDistance = 10;
     public Transform target;
+    public float speed = 2;
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, viewDistance);
+    }
 
     void Update()
     {
         float distance = Vector3.Distance(transform.position, target.position);
+        if (distance < viewDistance)
+        {
+            agent.SetDestination(target.position);
+        }
+        else
+        {
+
+        }
+
+
         if (distance < 3)
         {
             float angle = Vector3.Angle(transform.forward, target.transform.forward);
